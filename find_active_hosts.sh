e117bc546bd8bb329ddc7b423f24d5dfefc6e7bd
Tyler Milam <tmilam@iuhealth.org>
2016-07-28 15:13:14 -0400

    reversed logic on -eq 0. oops.

--- find_active_hosts.sh
+++ find_active_hosts.sh
@@ -3,8 +3,8 @@
 for ip in $(grep ^10.* /etc/hosts | awk '{ print $1 }'); do
         ping -c1 $ip &>/dev/null
         if [ $? -eq 0 ]
-                then echo $ip is down
+                then echo $ip is up 
         else
-                echo $ip is up
+                echo $ip is down 
         fi
 done
